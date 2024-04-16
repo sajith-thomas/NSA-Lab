ee8825fe3413756873f9ee6e728f4ec3740d5507
sajiththomas <145832902+sajith-thomas@users.noreply.github.com>
2024-04-16 13:35:38 +0530

    Create equal.sh

---
+++ Shell Scripting/equal.sh
@@ -0,0 +1,13 @@
+
+echo "checking wheather two numbers are equal"
+echo "enter the first number"
+read num1
+echo "enter the second number:"
+read num2
+if((num1==num2))
+ then
+         echo "they are equal"
+ else
+
+         echo "they are not equal"
+fi
